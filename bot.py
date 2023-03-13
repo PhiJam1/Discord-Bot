c0ab58ccafa61c027d7a0d6410b46a8986f4e0b6
Philip James <philipjames2004@gmail.com>
2023-03-12 20:01:44 -0500

    heh

--- bot.py
+++ bot.py
@@ -1,10 +1,9 @@
 from discord.ext import commands
 import discord
 import word
-BOT_TOKEN = "MTA4NDUyMzk4MTk0OTkwNzA2NQ.GECY0X.rz8jjOrj1w2J1J0Q0IfEKe7GW36mMwha-dMCTI"
+import cred
 bot = commands.Bot(command_prefix="$", intents=discord.Intents.all())
 CHANNEL_ID = 958397523217756213
-BIBMO_NAME = "bimbo#0660"
 
 #i've made a change
 
@@ -18,8 +17,8 @@ async def on_ready():
 async def on_message(message):
     channel = bot.get_channel(CHANNEL_ID)
     if (message.content == "$hello"):
-        if (message.author != BIBMO_NAME):
+        if (message.author != cred.BIBMO_NAME):
             await message.channel.send("got it from: " + str(message.author))
             await message.author.send('👋')
 
-bot.run(BOT_TOKEN)
+bot.run(cred.BOT_TOKEN)
