dd283277c9ccf5d6143cab9ae5a3c3a854155fd3
Philip James <philipjames2004@gmail.com>
2023-03-27 21:26:05 -0400

    Update bot.py

--- bot.py
+++ bot.py
@@ -6,8 +6,8 @@ import Member
 
 # Global Variables
 bot = commands.Bot(command_prefix="$", intents=discord.Intents.all())
-CHANNEL_ID = 609923829384984867
-SERVER_ID = 6949238293849844454
+CHANNEL_ID = 958397523217756213
+SERVER_ID = 958397522768982099
 file_name = "word_data.txt"
 #^using this means this code will only work for 1 server
 #that will need to be changed soon
@@ -19,20 +19,42 @@ loaded_data = False
 async def on_ready():
     print("Bot is online")
     channel = bot.get_channel(CHANNEL_ID)
-    await channel.send("Loading data...")
+    #await channel.send("Loading data...")
     #look through data files for members alr there. Then create any new ones
     #for new members that don't alr have members in it.
     ## fill up members list
     guild = bot.get_guild(SERVER_ID)
-    for user in guild.members:
-        members.append(Member.Member(user.name, user.id, [], [], 0, 0, 0))
-    await channel.send("...done")
+    
+    infile = open(file_name, "r")
+    for line in infile:
+        parts = line.split(", ")
+        user_name = parts[0]
+        user_id = int(parts[1])
+        terms_to_track = parts[2].split(":")
+        terms_to_track = terms_to_track[:len(terms_to_track) - 1]
+        term_total_use = parts[3].split("$")
+        term_total_use = term_total_use[:len(term_total_use) - 1]
+        for i in range(len(term_total_use)):
+            term_total_use[i] = int(term_total_use[i])
+        total_msg = int(parts[4])
+        total_questions = int(parts[5])
+        points = parts[6]
+        points = points[:len(points) - 1]
+        points = float(points)
+        #print(term_total_use)
+        members.append(Member.Member(user_name, user_id, terms_to_track, term_total_use, total_msg, total_questions, points))
+    infile.close()
+    loaded_data = True
+    #for user in guild.members:
+        #members.append(Member.Member(user.name, user.id, [], [], 0, 0, 0))
+    await channel.send("Data Loaded")
 
 @bot.event
 async def on_message(message):
-    channel = bot.get_channel(CHANNEL_ID)
+    #channel = message.channel
     #just used to see if anything works
     if (message.content == "$hello"):
+        save_all_data()
         if (message.author != cred.BIBMO_NAME):
             await message.channel.send("got it from: " + str(message.author))
             await message.author.send('👋')
@@ -58,9 +80,9 @@ async def get_members(message):
     for member in message.guild.members:
         await message.channel.send(member)
 async def add_word(message):
-    print("here")
     #get the word/phrase
     msg_content = message.content[9:]
+    msg_content = msg_content.lower()
     #do a check to see if its alr a word
     for word in members[0].terms_to_track:
         if (word == msg_content):
@@ -80,13 +102,12 @@ def scan_msg(message):
     if (message.content[0] != "$"):
         for member in members:
             if (member.user_name == message.author.name):
-                member.update_message_history(message.content)
+                member.update_message_history(message.content.lower())
     save_all_data()
 
 async def get_use_of_word(message):
     for member in members:
         await message.channel.send(member.get_total_stats())
-        print("line 84")
 async def get_user_stats(message):
     for member in members:
         if (message.content.find(str(member.user_id)) >= 0):
@@ -104,7 +125,6 @@ def save_all_data():
     for  member in members:
         file.write(member.get_data())
         file.write("\n")
-    file.write("END")
     #file.flush()
     file.close()
 bot.run(cred.BOT_TOKEN)
